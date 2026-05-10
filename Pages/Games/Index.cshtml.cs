dd895ffe06ec7fe8d09006b2ae32f66498f6b2bb
Vladimir Ventura <vladventura@outlook.es>
2026-05-10 18:21:08 -0400

    Adding filtering

--- Pages/Games/Index.cshtml.cs
+++ Pages/Games/Index.cshtml.cs
@@ -10,25 +10,30 @@ namespace Headlight.Pages.Games
     {
         [FromQuery(Name = "PlatformIdFilter")]
         public int PlatformIdFilter { get; set; }
+        [FromQuery(Name = "StatusIdFilter")]
+        public int StatusIdFilter { get; set; }
         [ViewData]
         public string PageTitle { get; set; } = "Games";
         public List<Game> AllGames { get; set; } = [];
         public void OnGet()
         {
+            IQueryable<Game>? query = context.Games.Include(o => o.Platform).Include(o => o.Status).OrderBy(g => g.Name);
+
             Platform? proposedFilteredPlatform = context.Platforms.Where(p => p.Id == PlatformIdFilter).FirstOrDefault();
-
-            var query = context.Games.Include(o => o.Platform).Include(o => o.Status).OrderBy(g => g.Name);
-
             if (proposedFilteredPlatform != null)
             {
-                AllGames = [.. query.Where(g => g.PlatformId == proposedFilteredPlatform.Id)];
+                query = query.Where(g => g.PlatformId == proposedFilteredPlatform.Id);
                 PageTitle = string.Format("{0} Games", proposedFilteredPlatform.Name);
             }
-            else
+
+            Status? proposedFilteredStatus = context.Statuses.Where(p => p.Id ==  StatusIdFilter).FirstOrDefault();
+            if (proposedFilteredStatus != null)
             {
-                AllGames = [.. query];
+                query = query.Where(g => g.StatusId == proposedFilteredStatus.Id);
+                PageTitle = string.Format("{0}: {1}", PageTitle, proposedFilteredStatus.Name);
             }
 
+            AllGames = [.. query];
         }
 
         public IActionResult OnPostAddGame()
