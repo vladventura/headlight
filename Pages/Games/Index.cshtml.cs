fcc002b97bee96577db30f5c808dbe7ba481269a
Vladimir Ventura <vladventura@outlook.es>
2026-05-10 17:49:53 -0400

    Adding filtered results functionality

--- Pages/Games/Index.cshtml.cs
+++ Pages/Games/Index.cshtml.cs
@@ -8,10 +8,27 @@ namespace Headlight.Pages.Games
 {
     public class IndexModel(AppDbContext context) : PageModel
     {
+        [FromQuery(Name = "PlatformIdFilter")]
+        public int PlatformIdFilter { get; set; }
+        [ViewData]
+        public string PageTitle { get; set; } = "Games";
         public List<Game> AllGames { get; set; } = [];
         public void OnGet()
         {
-            AllGames = [.. context.Games.Include(o => o.Platform).Include(o => o.Status).OrderBy(g => g.Name)];
+            Platform? proposedFilteredPlatform = context.Platforms.Where(p => p.Id == PlatformIdFilter).FirstOrDefault();
+
+            var query = context.Games.Include(o => o.Platform).Include(o => o.Status).OrderBy(g => g.Name);
+
+            if (proposedFilteredPlatform != null)
+            {
+                AllGames = [.. query.Where(g => g.PlatformId == proposedFilteredPlatform.Id)];
+                PageTitle = string.Format("{0} Games", proposedFilteredPlatform.Name);
+            }
+            else
+            {
+                AllGames = [.. query];
+            }
+
         }
 
         public IActionResult OnPostAddGame()
