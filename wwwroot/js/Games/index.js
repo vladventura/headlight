14232068f1687d51ae1806f4b481102efb68c21f
Vladimir Ventura <vladventura@outlook.es>
2026-05-16 20:35:44 -0400

    v0.6: Using ForerunnerDB for in-client typeahead search.

---
+++ wwwroot/js/Games/index.js
@@ -0,0 +1,60 @@
+﻿const forerunnerDb = new ForerunnerDB();
+const db = forerunnerDb.db("games-page");
+const collection = db.collection("games").deferredCalls(false);
+
+function onSearchInput() {
+    const lowerCaseValue = this.value.toLocaleLowerCase();
+    if (!lowerCaseValue) {
+        collection.find({}).forEach(r => {
+            document.getElementById(r["_id"]).style.display = "table-row";
+        });
+        return;
+    }
+    let r = new RegExp(lowerCaseValue, "i");
+    const includes = collection.find({
+        "name": r
+    });
+    const notIncludes = collection.find({
+        $not: {
+            "name": r
+        }
+    });
+
+    includes.forEach(r => {
+        document.getElementById(r["_id"]).style.display = "table-row";
+    });
+    notIncludes.forEach(r => {
+        document.getElementById(r["_id"]).style.display = "none";
+    });
+}
+
+// https://youmightnotneedjquery.com/#ready
+function ready(fn) {
+    if (document.readyState !== 'loading') {
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+
+const fillForerunnerDb = () => {
+    const itemsForCollection = [];
+    const tableRows = Array.from(targetTable.getElementsByTagName("tbody")[0].getElementsByTagName("tr"));
+    tableRows.forEach(tr => {
+        itemsForCollection.push({
+            _id: tr.id,
+            name: tr.cells[0].innerText
+        });
+    });
+    collection.insert(itemsForCollection);
+};
+
+const main = () => {
+    const targetTable = document.getElementById("games-table");
+    fillForerunnerDb();
+    document
+        .getElementById("searchInput")
+        .addEventListener("input", onSearchInput);
+};
+
+ready(main);
