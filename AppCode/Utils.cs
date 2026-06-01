d648867913ed2d74a9d706f57e20c301d71a2b57
Vladimir Ventura <vladventura@outlook.es>
2026-06-01 16:44:01 -0400

    Adding helper methods and enum

--- AppCode/Utils.cs
+++ AppCode/Utils.cs
@@ -1,8 +1,22 @@
-﻿namespace Headlight.AppCode
+﻿using Headlight.AppCode.Globals;
+
+namespace Headlight.AppCode
 {
     public class Utils
     {
         public static string GetDbConnectionString(string host, string port, string database, string username, string password)
         => string.Format("Host={0};Port={1};Database={2};Username={3};Password={4}", host, port, database, username, password);
+
+        public static string GetIconPartial(SvgIcon icon) => icon switch
+        {
+            SvgIcon.Delete => "_DeleteIconPartial",
+            _ => ""
+        };
+
+        public static string GetIconCssClass(SvgIcon icon) => icon switch
+        {
+            SvgIcon.Delete => "table-delete-icon",
+            _ => ""
+        };
     }
 }
