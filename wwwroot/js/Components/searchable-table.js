8d99505a56b6b4c0cd7f4b18486fdbe24b4dc77f
Vladimir Ventura <vladventura@outlook.es>
2026-06-03 22:32:51 -0400

    Adding js handlers for sorting functionality

--- wwwroot/js/Components/searchable-table.js
+++ wwwroot/js/Components/searchable-table.js
@@ -1,4 +1,38 @@
-﻿// https://youmightnotneedjquery.com/#ready
+﻿let currentPage = 1;
+let observer;
+let sentinelCopy;
+
+let isSortable = false;
+let isPaginateable = false;
+
+const getIsSortable = () => isSortable;
+const setIsSortable = (b) => isSortable = b;
+
+// Yes this is ridiculous
+const arrowUpSvg =
+    `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-short" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5"/>
+</svg>`;
+const arrowDownSvg =
+    `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-short" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4"/>
+</svg>`;
+
+let currentHeader = "";
+let sortDirection = "";
+
+const getCurrentHeader = () => currentHeader;
+const getSortDirection = () => sortDirection;
+
+const setCurrentHeader = (h) => {
+    currentHeader = h;
+};
+
+const setSortDirection = (sd) => {
+    sortDirection = sd;
+};
+
+// https://youmightnotneedjquery.com/#ready
 function ready(fn) {
     if (document.readyState !== 'loading') {
         fn();
@@ -7,28 +41,70 @@ function ready(fn) {
     }
 }
 
-let currentPage = 1;
-let observer;
+const destroyObserver = () => {
+    if (observer) {
+        observer.disconnect();
+    }
+    if (document.getElementById("sentinel")) {
+        document.getElementById("sentinel").remove();
+    }
+};
+
+const clearData = () => {
+    currentPage = 0;
+    const table = document.getElementById("searchable-table");
+    const tbody = table.getElementsByTagName("tbody")[0];
+    if (tbody) {
+        tbody.innerHTML = "";
+    }
+    if (isPaginateable) {
+        const tableParent = table.parentElement;
+        if (!tableParent.querySelector("#sentinel")) {
+            let newSentinel = document.createElement("div");
+            newSentinel.id = "sentinel";
+            newSentinel.className = "loading-sentinel";
+            newSentinel.innerText = "...";
+            tableParent.appendChild(newSentinel);
+        }
+        paginationSetup();
+    }
+};
+
+const getSortQueryString = () => { 
+    if (!getIsSortable() || !getCurrentHeader()) {
+        return "";
+    }
+    let sortField = getCurrentHeader().split('-')[0];
+    return `sortField=${sortField}&sortDirection=${getSortDirection()}`
+};
+
+const pullData = () => {
+    currentPage += 1;
+    let sortQueryString = getSortQueryString();
+    if (sortQueryString) {
+        sortQueryString = "&" + sortQueryString;
+    }
+    let queryParams = (location.search ? location.search + "&" : "?") + `incomingPage=${currentPage}&handler=Rows${sortQueryString}`;
+    const url = `${location.origin}${location.pathname}${queryParams}`;
+    fetch(url).then(r => r.text()).then(html => {
+        if (html.trim()) {
+            const table = document.getElementById("searchable-table");
+            const tbody = table.getElementsByTagName("tbody")[0];
+            if (tbody) {
+                tbody.innerHTML += html;
+            }
+        } else {
+            destroyObserver();
+        }
+    });
+};
 
 const paginationSetup = () => {
+    isPaginateable = true;
     observer = new IntersectionObserver(e => {
         e.forEach(entr => {
             if (entr.isIntersecting) {
-                currentPage += 1;
-                const queryParams = (location.search ? location.search + "&" : "?") + `incomingPage=${currentPage}&handler=Rows`;
-                const url = `${location.origin}${location.pathname}${queryParams}`;
-                fetch(url).then(r => r.text()).then(html => {
-                    if (html.trim()) {
-                        const table = document.getElementById("searchable-table");
-                        const tbody = table.getElementsByTagName("tbody")[0];
-                        if (tbody) {
-                            tbody.innerHTML += html;
-                        }
-                    } else {
-                        document.getElementById("sentinel").remove();
-                        observer.disconnect();
-                    }
-                });
+                pullData();
             }
         });
         }, {
@@ -39,12 +115,53 @@ const paginationSetup = () => {
     observer.observe(document.getElementById("sentinel"));
 };
 
+const sortSetup = () => {
+    setIsSortable(true);
+    sentinelCopy = document.getElementById("sentinel").cloneNode();
+    [... document.getElementsByClassName("clickable-header")]
+        .forEach(div => {
+            div.addEventListener("click", function () {
+                let activeHeader = getCurrentHeader();
+                let activeSortDirection = getSortDirection();
+                let arrowEl = document.getElementById(this.id + "-arrow");
+                if (activeHeader == this.id) {
+                    switch (activeSortDirection) {
+                        case "Asc":
+                            setSortDirection("Desc");
+                            arrowEl.innerHTML = arrowDownSvg;
+                            break;
+                        case "Desc":
+                            setSortDirection("");
+                            setCurrentHeader("");
+                            arrowEl.innerHTML = "";
+                            break;
+                        default:
+                            setSortDirection("Asc");
+                            arrowEl.innerHTML = arrowUpSvg;
+                            break;
+                    }
+                } else {
+                    if (getCurrentHeader()) {
+                        document.getElementById(getCurrentHeader() + "-arrow").innerHTML = "";
+                    }
+                    setCurrentHeader(this.id);
+                    setSortDirection("Asc");
+                    arrowEl.innerHTML = arrowUpSvg;
+                }
+                clearData();
+                pullData();
+            });
+        });
+};
 
 const main = () => {
     const typeAhead = new TypeAhead("tableitems", "searchInput", "searchable-table");
     typeAhead.loadTypeAhead();
     if (document.getElementById("sentinel")) {
         paginationSetup();
+    }
+    if (document.getElementsByClassName("clickable-header").length > 0) {
+        sortSetup();
     }
 };
 
